70c2d281c0bee8753bc88ef0e39f57a954a8b56f
developer <developer@penguin.home.box>
2014-04-23 22:45:01 -0700

    disabled windows ps

--- Logon.ps1
+++ Logon.ps1
@@ -2,43 +2,43 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-    $Host.UI.RawUI.WindowTitle = "Downloading PSWindowsUpdate..."
+#    $Host.UI.RawUI.WindowTitle = "Downloading PSWindowsUpdate..."
 
-    $psWindowsUpdateBaseUrl = "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/"
+ #   $psWindowsUpdateBaseUrl = "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/"
 
     #Fixes Windows Server 2008 R2 inexistent Unblock-File command Bug
-    if ($(Get-Host).version.major -eq 2)
-    {
-        $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "66095/1/PSWindowsUpdate_1.4.5.zip"
-    }
-    else
-    {
-        $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "41459/25/PSWindowsUpdate.zip"
-    }
+  #  if ($(Get-Host).version.major -eq 2)
+  #  {
+  #      $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "66095/1/PSWindowsUpdate_1.4.5.zip"
+  #  }
+  #  else
+  #  {
+  #      $psWindowsUpdateUrl = $psWindowsUpdateBaseUrl + "41459/25/PSWindowsUpdate.zip"
+  #  }
 
-    $psWindowsUpdatePath = "$ENV:Temp\PSWindowsUpdate.zip"
-    (new-object System.Net.WebClient).DownloadFile($psWindowsUpdateUrl, $psWindowsUpdatePath)
+   # $psWindowsUpdatePath = "$ENV:Temp\PSWindowsUpdate.zip"
+   # (new-object System.Net.WebClient).DownloadFile($psWindowsUpdateUrl, $psWindowsUpdatePath)
 
-    $Host.UI.RawUI.WindowTitle = "Installing PSWindowsUpdate..."
-    foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdatePath).Items())
-    {
-        $yesToAll = 16
-        (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
-    }
-    del $psWindowsUpdatePath
+   # $Host.UI.RawUI.WindowTitle = "Installing PSWindowsUpdate..."
+   # foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdatePath).Items())
+   # {
+   #     $yesToAll = 16
+   #     (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
+   # }
+   # del $psWindowsUpdatePath
 
-    Import-Module PSWindowsUpdate
+   # Import-Module PSWindowsUpdate
 
-    $Host.UI.RawUI.WindowTitle = "Installing updates..."
+   # $Host.UI.RawUI.WindowTitle = "Installing updates..."
 
-    Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
-    if (Get-WURebootStatus -Silent)
-    {
-        $Host.UI.RawUI.WindowTitle = "Updates installation finished. Rebooting."
-        shutdown /r /t 0
-    }
-    else
-    {
+   # Get-WUInstall -AcceptAll -IgnoreReboot -IgnoreUserInput -NotCategory "Language packs"
+  #  if (Get-WURebootStatus -Silent)
+   # {
+   #     $Host.UI.RawUI.WindowTitle = "Updates installation finished. Rebooting."
+   #     shutdown /r /t 0
+  #  }
+  #  else
+  #  {
         $Host.UI.RawUI.WindowTitle = "Downloading Cloudbase-Init..."
 
         $CloudbaseInitMsi = "$ENV:Temp\CloudbaseInitSetup_Beta.msi"
@@ -67,7 +67,7 @@ try
         $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
         $unattendedXmlPath = "$ENV:ProgramFiles (x86)\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml"
         & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/shutdown `/unattend:"$unattendedXmlPath"
-    }
+   # }
 }
 catch
 {
